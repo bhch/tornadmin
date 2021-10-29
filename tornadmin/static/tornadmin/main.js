cca5f195950e72221c95e55b5492fc55eddaeee3
Bharat Chauhan <tell.bhch@gmail.com>
2021-10-30 00:49:47 +0530

    Add checkboxes on list table

--- tornadmin/static/tornadmin/main.js
+++ tornadmin/static/tornadmin/main.js
@@ -16,3 +16,21 @@ $('#sideNavToggler').on('click', function(e) {
 $('#navOverlay').on('click', function(e) {
     toggleSideNav();
 });
+
+$('#list_check_all').on('change', function(e) {
+    $('.list-check').prop('checked', this.checked).trigger('change');
+});
+
+$('.list-check').on('change', function(e) {
+    var $row = $(this).parent().parent().parent();
+
+    if (this.checked) {
+        $row.addClass('table-active');
+    } else {
+        var $checkAll = $('#list_check_all');
+        if ($checkAll.prop('checked'))
+            $checkAll.prop('checked', false);
+        
+        $row.removeClass('table-active');
+    }
+});
